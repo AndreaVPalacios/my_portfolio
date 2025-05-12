0774e72e579f203bbb535c07482661abce8acb8c
AndreaVPalacios <andreita.estudia@gmail.com>
2025-05-12 00:20:40 -0400

    initial-commit

---
+++ front-end_lover/src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="flex justify-center items-center">
+      <p className="text-6xl font-[Uncage]">
+        Andrea Victoria Palacios Aguilera
+      </p>
+    </div>
+  );
+}
